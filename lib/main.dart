836c1704c8f913d404fd01ab1f4e0f7118125a83
anh <tnanh.vn>
2024-11-07 09:28:58 +0700

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:event_manager/event/event_view.dart';
 import 'package:flutter/material.dart';
+import 'event/event_view.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: EventView(),
     );
   }
 }
